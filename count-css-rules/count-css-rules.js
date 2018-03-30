52c5c7b8799a2e78ada8a81dd30d94533ccfb6b1
agentbishop <scottdurnell@gmail.com>
2018-03-30 16:59:11 -0500

    Updated to count MediaQuery rules
    Retooled js to include counting rules in Media Queries.

--- count-css-rules/count-css-rules.js
+++ count-css-rules/count-css-rules.js
@@ -1,44 +1,58 @@
 function countCSSRules() {
-    var log = '',
-        totalSelectorCount = 0, 
-        totalRuleCount = 0, 
-        totalSheetCount = 0;
+    var log = "";
+    var totalSelectorCount = 0, totalRuleCount = 0, totalSheetCount = 0;
 
     if (!document.styleSheets) {
         return;
     }
     
+    //loop through all the sheets
     for (var i = 0; i < document.styleSheets.length; i++) {
-        countSheet(document.styleSheets[i]);
-    }
-    
-    function countSheet(sheet) {
-        var selectorCount = 0, 
-            ruleCount = 0;
+        var sheet = document.styleSheets[i];
+
+        //only return local domain files
         if (sheet.href && (sheet.href.indexOf(document.domain) > 0)) {
+            var log;
+            var selectorCount = 0, ruleCount = 0;
+            var selectorCountMQ = 0, ruleCountMQ = 0;
 
-            if (sheet && sheet.cssRules) {
-                for (var j = 0, l = sheet.cssRules.length; j < l; j++) {
-                    if( !sheet.cssRules[j].selectorText) {
-                        continue;
+            //outer loop thru all rules
+            if (sheet.rules) {
+                for (var j = 0; j < sheet.rules.length; j++) {
+                    var rule = sheet.rules[j];
+
+                    if (rule) {
+                        if (rule.selectorText) {
+                            selectorCount += rule.selectorText.split(',').length;
+                            ruleCount += rule.cssText.split(";").length - 1;
+                        } else if (rule.cssRules) {
+                             //inner loop for media queries (they have their own set of cssRules)
+                            for (var k = 0; k < rule.cssRules.length; k++) {
+                                var rule2 = rule.cssRules[k];
+                                if (rule2) {
+                                    if (rule2.selectorText) {
+                                        selectorCountMQ += rule2.selectorText.split(',').length;
+                                        ruleCountMQ += rule2.cssText.split(";").length - 1;
+                                    }
+                                }
+                            }
+                        }
                     }
-                    selectorCount += sheet.cssRules[j].selectorText.split(',').length;
-                    ruleCount += sheet.cssRules[j].cssText.split(";").length - 1;
                 }
-
-                totalSelectorCount += selectorCount;
-                totalRuleCount += ruleCount;
-                totalSheetCount ++;
-
-                log += '\nFile: ' + (sheet.href ? sheet.href : 'inline <style> tag');
-                log += '\nSelectors: ' + selectorCount;
-                log += ', Rules: ' + ruleCount;
-                log += '\n--------------------------';
             }
+
+            totalSelectorCount += selectorCount + selectorCountMQ;
+            totalRuleCount += ruleCount + ruleCountMQ;
+            totalSheetCount ++;
+
+            log += '\nFile: ' + (sheet.href ? sheet.href : 'inline <style> tag');
+            log += '\nSelectors: ' + (selectorCount + selectorCountMQ) + ' [' + selectorCount + '; MQ: ' + selectorCountMQ + ']';
+            log += '; Rules: ' + (ruleCount + ruleCountMQ) + ' [' + ruleCount + '; MQ: ' + ruleCountMQ + ']';
+            log += '\n--------------------------';
         }
     }
-    console.log(log);
 
+    console.log(log);
     console.log('** TOTALS **');
     console.log('Total Sheet Count: ' + totalSheetCount);
     console.log('Total Selector Count:' + totalSelectorCount);
